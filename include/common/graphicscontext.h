d1c1dc59d509432d6a82b6bbf7806201b4ca25fb
pac-ac <138402580+pac-ac@users.noreply.github.com>
2023-10-01 01:19:42 -0700

    Add files via upload

---
+++ include/common/graphicscontext.h
@@ -0,0 +1,15 @@
+#ifndef __OS__COMMON__GRAPHICSCONTEXT_H
+#define __OS__COMMON__GRAPHICSCONTEXT_H
+
+#include <drivers/vga.h>
+
+namespace os {
+
+	namespace common {
+
+		typedef os::drivers::VideoGraphicsArray GraphicsContext;	
+	}
+}
+
+
+#endif
