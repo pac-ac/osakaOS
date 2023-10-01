9583b4f087e76696e242e4e5fb41c201c3dbb554
pac-ac <138402580+pac-ac@users.noreply.github.com>
2023-10-01 01:13:57 -0700

    Add files via upload

---
+++ include/gui/file_edit.h
@@ -0,0 +1,12 @@
+#ifndef __OS__MODE__FILE_EDIT_H
+#define __OS__MODE__FILE_EDIT_H
+
+#include <common/types.h>
+
+void fileTUI();
+
+void file(bool pressed, char key, bool ctrl, bool reset);
+
+void fileMain(bool pressed, char key, bool ctrl);
+
+#endif
