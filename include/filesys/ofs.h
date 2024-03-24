781640a5fbebd85acaf630bb652f6881814d806b
pac-ac <138402580+pac-ac@users.noreply.github.com>
2024-03-23 18:16:38 -0700

    Add files via upload

--- include/filesys/ofs.h
+++ include/filesys/ofs.h
@@ -5,6 +5,12 @@
 #include <drivers/ata.h>
 
 
+
+#define tableStartSector 512
+#define fileStartSector 513
+#define tagStartSector 513
+
+
 namespace os {
 
 	namespace filesystem {
@@ -14,20 +20,21 @@ namespace os {
 
 		common::uint32_t fnv1a(char*);
 		
-
 		common::uint32_t AddTable(char* name);
 		common::uint32_t RemoveTable(char* name);
 
+		common::uint32_t GetFileCount();
+		common::uint32_t GetFileSize(char* name);
+		common::uint32_t GetFileName(common::uint16_t fileNum, char* fileName);
+		
 		bool NewFile(char* name, common::uint8_t* file, 
 					 common::uint32_t size);
-
 
 		bool WriteFile(char* name, common::uint8_t* file, 
 					   common::uint32_t size);
 		bool WriteLBA(char* name, common::uint8_t* file, 
 					  common::uint32_t lba);
 		
-
 		bool ReadFile(char* name, common::uint8_t* file,	
 					  common::uint32_t size);
 		bool ReadLBA(char* name, common::uint8_t* file,	
@@ -35,8 +42,6 @@ namespace os {
 		
 
 		bool DeleteFile(char* name);
-
-		common::uint32_t GetFileSize(char* name);
 	}
 }
 
