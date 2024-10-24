95d243f8574afb673420c46daf5c7051104eed5e
pac-ac <138402580+pac-ac@users.noreply.github.com>
2024-10-24 06:46:15 -0700

    Add files via upload

--- include/mode/file_edit.h
+++ include/mode/file_edit.h
@@ -2,11 +2,12 @@
 #define __OS__MODE__FILE_EDIT_H
 
 #include <common/types.h>
+#include <filesys/ofs.h>
 
 void fileTUI();
 
-void file(bool pressed, char key, bool ctrl, bool reset);
+void file(bool pressed, char key, bool ctrl, bool reset, os::filesystem::FileSystem* file);
 
-void fileMain(bool pressed, char key, bool ctrl);
+void fileMain(bool pressed, char key, bool ctrl, os::filesystem::FileSystem* file);
 
 #endif
