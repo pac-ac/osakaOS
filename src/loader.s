32dc98c431fdc7eb596dad5735f54e5eb4608720
pac-ac <138402580+pac-ac@users.noreply.github.com>
2026-05-03 15:25:45 -0700

    Add files via upload

--- src/loader.s
+++ src/loader.s
@@ -1,11 +1,32 @@
 .set MAGIC, 0x1badb002
-.set FLAGS, (1<<0 | 1<<1)
+.set FLAGS, (1<<0 | 1<<1 | 1<<2)
 .set CHECKSUM, -(MAGIC + FLAGS)
+.set HEADER_ADDR, 0
+.set LOAD_ADDR, 0
+.set LOAD_END_ADDR, 0
+.set BSS_END_ADDR, 0
+.set ENTRY_ADDR, 0
+.set MODE_TYPE, 1
+.set WIDTH, 0
+.set HEIGHT, 0
+.set DEPTH, 0
+#.set WIDTH, 640
+#.set HEIGHT, 480
+#.set DEPTH, 8
 
 .section .multiboot
 	.long MAGIC
 	.long FLAGS
 	.long CHECKSUM
+	.long HEADER_ADDR
+	.long LOAD_ADDR
+	.long LOAD_END_ADDR
+	.long BSS_END_ADDR
+	.long ENTRY_ADDR
+	.long MODE_TYPE
+	.long WIDTH
+	.long HEIGHT
+	.long DEPTH
 
 
 .section .text
@@ -34,5 +55,5 @@ _stop:
 	jmp _stop
 
 .section .bss
-.space 2*1024*1024	; # 2 MiB
+.space 4*1024*1024	; # 4 MiB
 kernel_stack:
